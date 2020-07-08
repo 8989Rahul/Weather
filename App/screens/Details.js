df8a977cdb536081537f79ac0174fde21d009afd
8989Rahul <daghalerahul0@gmail.com>
2020-07-08 20:19:41 +0530

    weather api in integrated with apikey

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+const apikey = "b1e34ab4f469ae8afd7b4e653c1e16da";
 class Details extends React.Component {
   componentDidMount() {
-    fetch(``);
+    // api.openweathermap.org/data/2.5/weather?zip=94040,us
+    const zipCode = 94040;
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?appid=${apikey}&zip=${zipCode}`
+    )
+      .then((res) => res.json())
+      .then((res) => console.log("res", res))
+      .catch((err) => console.log("err", err));
   }
 
   render() {
